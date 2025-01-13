7ced13ae050f9958b3aeede40809e5504e11ebfb
latifa <taybilatifa46@gmail.com>
2025-01-13 16:42:03 +0100

    registre

---
+++ auth/registre.php
@@ -0,0 +1,26 @@
+<?php
+include '../database/config.php';
+
+if(isset($_POST['registre'])){
+    $nom = $_POST['nom'];
+    $email = $_POST['email'];
+    $motDePasse = $_POST['motDePasse'];
+    $role = $_POST['role'];
+
+    $db=new database();
+    $pdo=$db->getConn($pdo);
+
+    $motDePasse_hash = password_hash($motDePasse, PASSWORD_BCRYPT);
+    $stmt = $pdo->prepare("INSERT INTO utilisateur(nom, email, mot_de_passe,role)VALUES(:nom, :email, :mot_de_passe, :role)");
+    if($stmt->execute([
+        ':nom'=>$nom,
+        ':email'=>$email,
+        ':mot_de_passe'=>$motDePasse_hash,
+        ':role'=>$role
+    ])){
+        header('location: ./registrationPage.php');
+    }else{
+        header('location: ./registrationPage.php');
+    }
+}
+?>
