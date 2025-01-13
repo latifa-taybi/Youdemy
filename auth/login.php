82f8e05f1fa99e2d9b375cf16f9b82f7462867ab
latifa <taybilatifa46@gmail.com>
2025-01-13 16:42:33 +0100

    login

---
+++ auth/login.php
@@ -0,0 +1,35 @@
+<?php
+session_start();
+
+require_once '../database/config.php';
+
+if(isset($_POST['login'])){
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    $db = new database();
+    $pdo = $db->getConn($pdo);
+
+    $stmt = $pdo->prepare("SELECT * FROM utilisateur WHERE email=:email");
+    $stmt->execute([
+        ':email'=>$email
+    ]);
+    $utilisateur = $stmt->fetch();
+    if($utilisateur && password_verify($password, $utilisateur['mot_de_passe'] )){
+        $_SESSION['user_nom'] = $utilisateur['nom'];
+        $_SESSION['user_email'] = $utilisateur['email'];
+        $_SESSION['user_role'] = $utilisateur['role'];
+        $_SESSION['user_statut'] = $utilisateur['statut'];
+        if( $utilisateur['role']=='Etudiant'){
+            header('location: ../etudiant/etudiantPage.php');
+        }else if($utilisateur['role']=='Enseignant'){
+            header('location: ../enseignant/enseignantPage.php');
+        }else{
+            header('location: ../administrateur/admin.php');
+        }
+    }else{
+        header('location: ./registrationPage.php');
+    }
+}
+
+?>
